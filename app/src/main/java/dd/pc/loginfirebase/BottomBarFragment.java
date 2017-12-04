15b95ee2dc60f4855d3543a8218ee0325f7342af
Notorious <noto.scott1@gmail.com>
2017-12-04 13:20:01 -0500

    Initial commit

---
+++ app/src/main/java/dd/pc/loginfirebase/BottomBarFragment.java
@@ -0,0 +1,14 @@
+package dd.pc.loginfirebase;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by hussa on 10/15/2017.
+ */
+
+public class BottomBarFragment extends Fragment {
+
+
+
+}
+
