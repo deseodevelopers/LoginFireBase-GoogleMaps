94bcf9afdb673e4a211cc278be32e8eba4311386
Notorious <noto.scott1@gmail.com>
2017-12-19 10:44:48 -0500

    Maps activity 1

--- app/src/main/java/dd/pc/loginfirebase/MapsActivity.java
+++ app/src/main/java/dd/pc/loginfirebase/MapsActivity.java
@@ -4,8 +4,10 @@ package dd.pc.loginfirebase;
  * Created by PC on 11/30/2017.
  */
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,9 +16,82 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+
+// This package was previously defined in the android manifest
+//import deseodevs.com.loginfirebase.R;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+//public class MapsActivity extends SupportMapFragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    /**
+     * Represents a geographical location in the past.
+     */
+    private Location mLastLocation;
+    private String mLatitudeLabel, mLongitudeLabel;
+    private TextView mLatitudeText, mLongitudeText;
+    /**
+     * Represents a geographical location in the present.
+     */
+    private Location mCurrentLocation;
+
+    /**
+     * Displays the location address.
+     */
+    private TextView mLocationAddressTextView;
+
+
+    /**
+     * Tracks whether the user has requested an address. Becomes true when the user requests an
+     * address and false when the address (or an error message) is delivered.
+     */
+    private boolean mAddressRequested;
+
+    /**
+     * The formatted location address.
+     */
+    private String mAddressOutput;
+
+    /**
+     * Receiver registered with this activity to get the response from FetchAddressIntentService.
+     */
+    private MainActivity.AddressResultReceiver mResultReceiver;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +117,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        setUpMap();
 
 
         // Add a marker in Sydney and move the camera
         //LatLng currentLocation = new LatLng(33.7756, -84.3963);
-        LatLng currentLocation = new LatLng(33.950761, -84.243963);
-        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+
+
+        //LatLng currentLocation = new LatLng(Integer.parseInt(String mLatitudeText), Integer.parseInt(String mLongitudeText));
+
+        //String latitude = mLatitudeText;
+        //String longitude = mLongitudeText;
+        //LatLng currentLocation = new LatLng(Integer.parseInt(String latitude), Integer.parseInt(String longitude));
+
+        //mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+        //mMap.moveCamera(CameraUpdateFactory.zoomTo(17));
 
     }
+
+    public void setUpMap(){
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMyLocationEnabled(true);
+        mMap.setTrafficEnabled(true);
+        mMap.setIndoorEnabled(true);
+        mMap.setBuildingsEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+
 }
